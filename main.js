7767c2b777aa91f7b000152a8525ea82d892bd9c
Edward Smith <65724195+esmith80@users.noreply.github.com>
2024-06-11 09:12:29 -0600

    move data and JS logic into separate files

---
+++ main.js
@@ -0,0 +1,114 @@
+var TDSort = (function () {
+  // the column index on which we are sorting
+  var sortIndex = -1;
+  // was the last sort a reverse sort?
+  var reverseSort = false;
+  // not going to try too hard for browser compatibility - just check for IE or non-IE
+  var mTextKey = document.all ? "innerText" : "textContent";
+  var mTableID = "";
+  var mHeaderRowID = "";
+  var mIndexCol = 0;
+  // get the data that corresponds to this page
+  const pathArray = window.location.pathname.split("/");
+  const pageName = pathArray.pop().split(".")[0];
+  const mData = getPageData(pageName);
+
+  // initialize the page
+  function init(inTableID, inHeaderRowID) {
+    mTableID = inTableID;
+    mHeaderRowID = inHeaderRowID;
+
+    // install an onClick handler for each column header
+    var theRow = document.getElementById(mHeaderRowID);
+    var getSortFn = function (inIndex) {
+      return function () {
+        sortByColumn(inIndex);
+      };
+    };
+
+    if (theRow) {
+      for (var i = 0, iLen = theRow.cells.length; i < iLen; i++) {
+        if (
+          i != mIndexCol &&
+          mColumns[i].Key != "_PlayerImage" &&
+          mColumns[i].Key != "_HitmanImage"
+        ) {
+          theRow.cells[i].onclick = getSortFn(i);
+          theRow.cells[i].style.cursor = "pointer";
+        }
+      }
+    }
+
+    // put a reference to each row in the data, if we haven't already
+    var theRows = document.getElementById(mTableID).rows;
+
+    for (var i = 0, iLen = mData.length; i < iLen; i++)
+      mData[i].Row = theRows[i + 1];
+  }
+
+  // sort fn
+  function sortRow(a, b) {
+    var aVal = a.Columns[sortIndex].SortValue;
+    var bVal = b.Columns[sortIndex].SortValue;
+
+    if (aVal === null || bVal === null) {
+      // for equal values, fall back on the row index
+      if (aVal === bVal) return a.Index - b.Index;
+
+      return aVal === null ? -1 : 1;
+    }
+
+    if (aVal < bVal) return -1;
+    else if (aVal > bVal) return 1;
+
+    // for equal values, fall back on the row index
+    return a.Index - b.Index;
+  }
+
+  function sortByColumn(inIndex) {
+    if (mData.length == 0) return;
+
+    if (inIndex == sortIndex)
+      reverseSort =
+        !reverseSort; // sorting the same column, again, so reverse the current sort
+    else reverseSort = false; // if sorting on a new column, always reset to forward sort
+
+    sortIndex = inIndex;
+
+    var theTable = document.getElementById(mTableID);
+    var theParent = theTable.rows[0].parentNode;
+
+    // remove all rows, in current sort order (appears to be the fastest way)
+    for (var i = 0, iLen = mData.length; i < iLen; i++)
+      theParent.removeChild(mData[i].Row);
+
+    // sort the rows
+    mData.sort(sortRow);
+
+    if (reverseSort) mData.reverse();
+
+    // put the rows back in the new sorted order
+    // there may or may not be an empty row followed by a sum and average rows, so for an easy solution insert the
+    // rows before the header row, then pop the header row off and put it back in front of the first row
+    var theHeader = theParent.rows[0];
+
+    for (var i = 0, iLen = mData.length; i < iLen; i++) {
+      // set the row's class to maintain even/odd row shading
+      mData[i].Row.className = i % 2 ? "odd" : "even";
+      theParent.insertBefore(mData[i].Row, theHeader);
+    }
+
+    theParent.removeChild(theHeader);
+    theParent.insertBefore(theHeader, mData[0].Row);
+
+    // update the index column
+    if (mIndexCol >= 0) {
+      for (var i = 0, iLen = mData.length; i < iLen; i++)
+        mData[i].Row.cells[mIndexCol][mTextKey] = "" + (i + 1);
+    }
+  }
+
+  return {
+    init: init,
+  };
+})();
