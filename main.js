5d2167396b9bc629ea077c171eb3c1348652ba85
Edward Smith <65724195+esmith80@users.noreply.github.com>
2024-06-12 01:47:14 -0600

    move all JS into one file for clarity

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ const searchParams = new URLSearchParams(queryString);
 // only allowed param is time 'period'; if this changes, update this code
 const period = searchParams.get('period') || '2024';
 
+const dataSets = [
+  { Period: "2024", Name: "2024" },
+  { Period: "all_time", Name: "All Time" },
+]
+
+const mColumns = [
+  { Key: "_Index", Name: "#" },
+  { Key: "Name", Name: "Name" },
+  { Key: "Buyins", Name: "Buy-ins" },
+  { Key: "RebuysCount", Name: "Rebuys" },
+  { Key: "Hits", Name: "Hits" },
+  { Key: "TotalWinnings", Name: "Total Winnings" },
+  { Key: "TotalCost", Name: "Total Cost" },
+  { Key: "TotalTake", Name: "Total Take" },
+  { Key: "TimesPlaced", Name: "Times Placed" },
+  { Key: "First", Name: "1st" },
+  { Key: "Second", Name: "2nd" },
+  { Key: "Third", Name: "3rd" },
+  { Key: "AveragePlaced", Name: "Average Placed" },
+  { Key: "OnTheBubble", Name: "Bubble" },
+  { Key: "AverageHits", Name: "Average Hits" },
+];
+
 async function initializePage() {
   console.log('initializing page')
   await setData();
