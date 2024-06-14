3fdc9edf2954e38909bcd1b7ac73aad6846196a8
Edward Smith <65724195+esmith80@users.noreply.github.com>
2024-06-14 10:56:18 -0600

    fix path to reports to work on github pages

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function processQueryparams() {
 async function loadReports() {
   try {
     // Fetch the JSON file
-    const response = await fetch('_reports.json');
+    const response = await fetch('./_reports.json');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
